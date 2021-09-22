e11c2bd01dfc85ed644ebd5ceb3343f6e760ef57
Thiago <tms1_tms2@hotmail.com>
2021-09-22 14:46:34 -0300

    Create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href="/">
+          <a>
+            <Image src="/images/logo.svg" alt="logo" width="239" height="27" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
