1f03d4e9567beb71a8687d5f3f03c9d4e3d74d6f
Thiago <tms1_tms2@hotmail.com>
2021-09-22 19:21:39 -0300

    GetStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,11 @@ import { GetStaticProps } from 'next';
 
 import Head from 'next/head';
 
+import Prismic from '@prismicio/client';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -26,7 +31,7 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
   return (
     <>
       <Head>
@@ -48,9 +53,44 @@ export default function Home(): JSX.Element {
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 1,
+    }
+  );
 
-//   // TODO
-// };
+  const { next_page } = postsResponse;
+
+  const results = postsResponse.results.map(post => {
+    const first_publication_date = format(
+      new Date(post.first_publication_date),
+      'dd MMM y',
+      {
+        locale: ptBR,
+      }
+    );
+
+    return {
+      uid: post.uid,
+      first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page,
+    results,
+  };
+
+  return {
+    props: { postsPagination },
+  };
+};
